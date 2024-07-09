b8467bfc9fe2f3aedd1744fceddbb2cc3bcd7fa1
SREEHARI J <Sreeharij2003@gmail.com>
2024-07-09 23:08:51 +0530

    Time: 386 ms (80.99%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT author_id AS id FROM views WHERE author_id = viewer_id ORDER BY author_id;
+SELECT author_id AS id FROM views WHERE author_id = viewer_id GROUP BY author_id ORDER BY author_id;
